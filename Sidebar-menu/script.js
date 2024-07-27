b69126717f83baacfcea4f74f8886a73588198d4
Jafar Soltani <js225iw@student.lnu.se>
2024-07-27 02:30:16 +0200

    done sidebar project

--- Sidebar-menu/script.js
+++ Sidebar-menu/script.js
@@ -4,3 +4,15 @@ const menu = document.querySelector('.menu-toggle')
 menu.onclick = function () {
     nav.classList.toggle('open')
 }
+
+const list = document.querySelectorAll('.list')
+function activeLink(){
+    list.forEach((item) => {
+        item.classList.remove('active')
+    });
+    this.classList.add('active')
+}
+
+list.forEach((item) => {
+    item.addEventListener('click', activeLink)
+})
